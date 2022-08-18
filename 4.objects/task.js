5bfa99682864ce015ed90c93cb883153657ca4a0
Maria <nikmaria15@gmail.com>
2022-08-18 14:06:09 +0500

    task 4.objects is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  return this.name = name, this.gender = gender, this.age = age;
 }
+
+let student1 = new Student("Ivan", "male", 24);
+let student2 = new Student("Mark", "male", 27);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = [...marks]
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.every((item) => sum += item);
+  return this.average = sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
