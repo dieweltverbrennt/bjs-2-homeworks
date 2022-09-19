b48dad22f1aa0dc449b6bb8e8949bbf264cdcb7b
Maria <nikmaria15@gmail.com>
2022-09-19 12:37:12 +0500

    update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let objectInCache = cache.find(item => item.hash === hash);
+    if(objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args);
+    cache.push({hash, result});
+    if(cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result; 
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  
+  function wrapper(...args) {
+    if(!timeoutId) {
+      wrapper.count++
+      func(...args);
+    }
+    else {
+      clearTimeout(timeoutId);
+      wrapper.allCount++;
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+      }, delay);
+  }
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+  return wrapper;
 }
